6651b7371157a4000a4634e29a9f5dc971da27e1
Subodh kumar <79571881+Subodhkr360@users.noreply.github.com>
2026-03-29 09:55:21 +0530

    Update build properties for OnePlus device

--- lineage_caihong.mk
+++ lineage_caihong.mk
@@ -25,8 +25,8 @@ EXTRA_UDFPS_ANIMATIONS := false
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi_64-user 16 BP2A.250605.015 1768876946191 release-keys" \
-    BuildFingerprint=OnePlus/OPD2403IN/OP5DAAL1:16/UKQ1.231108.001/U.R4T3.2f5f456-deb173-e28402:user/release-keys \
+    BuildDesc="qssi_64-user 16 BP2A.250605.015 1773392195226 release-keys" \
+    BuildFingerprint=OnePlus/OPD2403IN/OP5DAAL1:16/UKQ1.231108.001/U.R4T3.39a7a65-16b7852-170fdcc:user/release-keys \
     DeviceName=OP5DAAL1 \
     DeviceProduct=OPD2403 \
     SystemDevice=OP5DAAL1 \
