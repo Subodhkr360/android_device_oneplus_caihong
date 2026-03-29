deda8bd4a7aea4ed90274f40561f5b838ec9ff7c
Subodh kumar <79571881+Subodhkr360@users.noreply.github.com>
2026-03-29 11:49:07 +0530

    Remove TARGET_INCLUDE_VIPERFX setting
    Remove TARGET_INCLUDE_VIPERFX from configuration.

--- lineage_caihong.mk
+++ lineage_caihong.mk
@@ -31,7 +31,6 @@ HBM_NODE := /sys/kernel/oplus_display/hbm
 GPU_FREQS_PATH := /sys/class/kgsl/kgsl-3d0/devfreq/available_frequencies
 GPU_MIN_FREQ_PATH := /sys/class/kgsl/kgsl-3d0/devfreq/min_freq
 TORCH_STR_SUPPORTED := true
-TARGET_INCLUDE_VIPERFX := true
 TARGET_INCLUDES_LOS_PREBUILTS := true
 PERF_GOV_SUPPORTED := true
 PERF_DEFAULT_GOV := walt
